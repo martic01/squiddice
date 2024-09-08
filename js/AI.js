9fa522914ea411b787460c053bea45f32e4cac1d
matthew aboyade <aboyadematthew@gmail.com>
2024-09-08 05:21:21 -0700

    add increased ai time

--- js/AI.js
+++ js/AI.js
@@ -66,15 +66,15 @@ function rollAI() {
             if (totalScore >= goalThreshold) {
                 setTimeout(function () {
                     $(".save").trigger('click');
-                }, 800);
+                }, 2500);
                 aiRolling = false;
             }
         }
 
        if(aiRolling) {
-            setTimeout(rollAI, 1300);
+            setTimeout(rollAI, 1600);
         }
-    }, 800);
+    }, 3500);
 }
 
 
