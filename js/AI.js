7d6b067be2906c8fc91347e8bc6102ca334f6cd0
matthew aboyade <aboyadematthew@gmail.com>
2025-01-21 22:35:35 +0100

    add levels html and css update

--- js/AI.js
+++ js/AI.js
@@ -42,8 +42,8 @@ let aiRolling = true;
 function rollAI() {
     let rollResult = dieNumber();
     let totalScore = parseInt($("#count").text());
-    let goalThreshold = 11;
-    let dontRoll1 = 6;
+    let goalThreshold = 23;
+    let dontRoll1 = 18;
     let button1 = document.querySelector("#roll");
     let button2 = document.querySelector(".resimg");
     let button3 = document.querySelector(".save");
