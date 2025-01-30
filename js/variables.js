dc66067d2f54c533b821181c0ebe0900a0710821
matthew aboyade <aboyadematthew@gmail.com>
2025-01-30 02:53:10 +0100

    add change bar numbers

--- js/variables.js
+++ js/variables.js
@@ -1,34 +1,35 @@
+let level = "Easy";
 let playerVSai;
 let timeTurn;
 let timeOut;
 let rollOne;
 let active;
 let goal;
-let bar = 0
-let back = false
-let SDcheck = false;
+let back = false;
 let showTurn = true;
 let aiRolling = true;
-let playerSw = 1;
 let off = true;
 let SC1 = true;
 let SC2 = true;
 let SC3 = true;
 let SC4 = true;
+let goalThreshold = 10;
 let playerResult = 0;
 let playerGoal1 = 0;
 let playerGoal2 = 0;
-let goalThreshold = 10;
+let newState2 = 1;
 let dontRoll1 = 6;
+let playerSw = 1
+let newState = 1;
+let time = 400;
 let stages = 1;
 let coin = 30;
 let state = 1;
-let newState = 1;
-let newState2 = 1;
-let time = 400;
-let level = "Easy";
-let gA = [10, 20, 30, 40, 100, 80, 350];
-let bL = [5,10,15,20]
-let count = [0,0,0,0];
+let bar = 0;
 let recordAi = [];
 let recordHum = [];
+let bL = [5,15,20,25];
+let count = [0,0,0,0];
+let gA = [10, 20, 30, 40, 100, 80, 350];
+
+
