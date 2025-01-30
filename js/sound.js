b3b0a4f6d478e505f6da059227499c3cb70aa7c0
matthew aboyade <aboyadematthew@gmail.com>
2025-01-30 01:14:10 +0100

    add two more sound and made entry sound

--- js/sound.js
+++ js/sound.js
@@ -9,6 +9,8 @@ $(document).ready(function () {
                 $(".audio").html(`<audio class="aud" src="audio/favorite.mp3" autoplay loop> `)
                 SC1 = false
                 SC2 = true
+                SC3 = true
+                SC4 = true
                 active = 1
                 SDcheck = true
                 off = false
@@ -17,8 +19,10 @@ $(document).ready(function () {
                 $('.sdsw').text('OFF');
             } else {
                 $(".audio").html(``)
-                SC2 = true
                 SC1 = true
+                SC2 = true
+                SC3 = true
+                SC4 = true
                 active = 0
                 SDcheck = false
                 off = true
@@ -29,8 +33,10 @@ $(document).ready(function () {
         } else if (index === 1) {
             if (SC2) {
                 $(".audio").html(`<audio class="aud" src="audio/drill.mp3" autoplay loop> `)
-                SC2 = false
                 SC1 = true
+                SC2 = false
+                SC3 = true
+                SC4 = true
                 active = 2
                 SDcheck = true
                 off = false
@@ -41,6 +47,60 @@ $(document).ready(function () {
                 $(".audio").html(``)
                 SC1 = true
                 SC2 = true
+                SC3 = true
+                SC4 = true
+                active = 0
+                SDcheck = false
+                off = true
+                $(this).removeClass('coll');
+                $(this).find('p span').text('🔈');
+                $('.sdsw').text('ON');
+            }
+        } else if (index === 2) {
+            if (SC3) {
+                $(".audio").html(`<audio class="aud" src="audio/squid amapiano.mp3" autoplay loop> `)
+                SC1 = true
+                SC2 = true
+                SC3 = false
+                SC4 = true
+                active = 3
+                SDcheck = true
+                off = false
+                $(this).addClass('coll');
+                $(this).find('p span').text('🔊');
+                $('.sdsw').text('OFF');
+            } else {
+                $(".audio").html(``)
+                SC1 = true
+                SC2 = true
+                SC3 = true
+                SC4 = true
+                active = 0
+                SDcheck = false
+                off = true
+                $(this).removeClass('coll');
+                $(this).find('p span').text('🔈');
+                $('.sdsw').text('ON');
+            }
+        } else if (index === 3) {
+            if (SC4) {
+                $(".audio").html(`<audio class="aud" src="audio/mingle squid.mp3" autoplay loop> `)
+                SC1 = true
+                SC2 = true
+                SC3 = true
+                SC4 = false
+                active = 4
+                SDcheck = true
+                off = false
+                $(this).addClass('coll');
+                $(this).find('p span').text('🔊');
+                $('.sdsw').text('OFF');
+            } else {
+                $(".audio").html(``)
+                SC1 = true
+                SC2 = true
+                SC3 = true
+                SC4 = true
                 active = 0
                 SDcheck = false
                 off = true
@@ -49,22 +109,31 @@ $(document).ready(function () {
                 $('.sdsw').text('ON');
             }
         }
+
     });
 
     $(".off").click(function () {
         if (!SDcheck) {
             if (off) {
-                active = Math.floor(Math.random() * 2) + 1
+                active = Math.floor(Math.random() * 4) + 1
                 if (active === 1) {
                     $('.sd1').trigger('click');
                 } else if (active === 2) {
                     $('.sd2').trigger('click');
+                } else if (active === 3) {
+                    $('.sd3').trigger('click');
+                } else if (active === 4) {
+                    $('.sd4').trigger('click');
                 }
             } else {
                 if (active === 1) {
                     $('.sd1').trigger('click');
                 } else if (active === 2) {
                     $('.sd2').trigger('click');
+                } else if (active === 3) {
+                    $('.sd3').trigger('click');
+                } else if (active === 4) {
+                    $('.sd4').trigger('click');
                 }
             }
         } else {
@@ -74,9 +143,14 @@ $(document).ready(function () {
                 $('.sd1').trigger('click');
             } else if (active === 2) {
                 $('.sd2').trigger('click');
+            } else if (active === 3) {
+                $('.sd3').trigger('click');
+            } else if (active === 4) {
+                $('.sd4').trigger('click');
             }
             $('.sdsw').text('ON');
             active = 0
         }
     });
+    $('.sd3').trigger('click');
 });
